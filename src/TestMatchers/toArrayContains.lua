52f93b61281e62a54ac00405f6f8588e2b114efd
Anonymous Author <roflection-bot@roblox.com>
2021-01-27 09:48:23 -0500

    Feature/dev print (#62)
    * rename devPrint to inspect
    
    * add dev print functionality to print and inspect
    
    * Update src/TestMatchers/devPrint.lua
    
    
    * Update src/TestMatchers/devPrint.lua
    
    * fix devPrint

--- src/TestMatchers/toArrayContains.lua
+++ src/TestMatchers/toArrayContains.lua
@@ -1,5 +1,5 @@
 local arrayContains = require(script.Parent.Parent.luaUtils.arrayContains)
-local devPrint = require(script.Parent.devPrint)
+local inspect = require(script.Parent.inspect)
 
 local function toArrayContains(tbl, item)
 	local success = arrayContains(tbl, item)
@@ -7,7 +7,7 @@ local function toArrayContains(tbl, item)
 	local message = ""
 	if not success then
 		-- TODO: find way to pretty print variables into output and expect it
-		message = "recieved tbl: " .. devPrint(item) .. " expected item to be in table: " .. devPrint(tbl)
+		message = "recieved tbl: " .. inspect(item) .. " expected item to be in table: " .. inspect(tbl)
 		message = "item not found in tbl"
 	end
 
