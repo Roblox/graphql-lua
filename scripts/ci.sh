dbbfbfa0cffa84237cd441ca70a8dee9921f802b
Anonymous Author <roflection-bot@roblox.com>
2021-02-04 10:46:06 -0800

    add test runners to run with or without fuzz tests (#121)

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -7,4 +7,4 @@ rojo build test-bundle.project.json --output graphql.rbxmx
 roblox-cli analyze test-bundle.project.json
 selene src
 
-roblox-cli run --load.model graphql.rbxmx --run scripts/spec.lua
+roblox-cli run --load.model graphql.rbxmx --run scripts/run-unit-tests.lua
